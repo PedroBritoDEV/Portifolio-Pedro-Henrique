311e46ee8a7ef2ec4a65efbfb0dea79942545cb2
PedroBritoDEV <contapdr10@gmail.com>
2023-09-24 17:15:51 -0300

    dist com a versão mobile do 'sobreView.vue'

--- assets/vue-splide.esm-2ec3c658.js
+++ assets/vue-splide.esm-2ec3c658.js
@@ -1,4 +1,4 @@
-import{g as Vt,h as rn,i as lr,j as dr,k as Er,l as gr,m as an,r as hr,o as st,n as on,w as un,q as ct,s as _r,a as Nn,v as mr,x as Ar,d as Sr}from"./index-b78cc0d7.js";function sn(e,n){for(var t=0;t<n.length;t++){var r=n[t];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function Tr(e,n,t){return n&&sn(e.prototype,n),t&&sn(e,t),Object.defineProperty(e,"prototype",{writable:!1}),e}/*!
+import{g as Vt,h as rn,i as lr,j as dr,k as Er,l as gr,m as an,r as hr,o as st,n as on,w as un,q as ct,s as _r,c as Nn,v as mr,x as Ar,d as Sr}from"./index-c2a60db8.js";function sn(e,n){for(var t=0;t<n.length;t++){var r=n[t];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function Tr(e,n,t){return n&&sn(e.prototype,n),t&&sn(e,t),Object.defineProperty(e,"prototype",{writable:!1}),e}/*!
  * Splide.js
  * Version  : 4.1.3
  * License  : MIT
